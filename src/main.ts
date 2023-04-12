7cc18ec2975d1a175bdb1ab19d2dfeb2449293e0
icepick4 <remi.jara@laposte.net>
2023-04-12 19:07:42 +0200

    adding light + shadow + little event click

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,23 @@ let renderer: THREE.WebGLRenderer,
     controls: OrbitControls;
 
 const render = () => {
-    requestAnimationFrame(render);
+    window.requestAnimationFrame(render);
+    animateSphere();
     renderer.render(scene, camera);
 };
 
 const animateSphere = () => {
-    requestAnimationFrame(animateSphere);
-    sphere.rotation.y += 0.004;
+    sphere.rotation.y += 0.0025;
+};
+
+const fullRotation = () => {
+    //make one full rotation then stop
+    const rotation = setInterval(() => {
+        sphere.rotation.y += 0.025;
+    }, 1);
+    setTimeout(() => {
+        clearInterval(rotation);
+    }, 1000);
 };
 
 scene = new THREE.Scene();
@@ -28,6 +38,7 @@ camera.position.z = 5;
 
 renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 controls = new OrbitControls(camera, renderer.domElement);
@@ -36,12 +47,14 @@ controls.enablePan = false;
 controls.minDistance = 1;
 controls.maxDistance = 10;
 
-// lights
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-hemiLight.position.set(0, 200, 0);
 const dirLight = new THREE.PointLight(0xffffff, 2, 400);
-dirLight.position.set(0, 200, 100);
+dirLight.position.set(0, 20000, 1000);
 dirLight.castShadow = true;
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+dirLight.shadow.camera.near = 0.5;
+dirLight.shadow.camera.far = 500;
+dirLight.shadow.radius = 30;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000, 0);
@@ -51,26 +64,40 @@ const geometry = new THREE.SphereGeometry(1, 32, 32);
 const material = new THREE.MeshBasicMaterial({
     map: earthTexture
 });
+material.map!.anisotropy = 16;
 const sphere = new THREE.Mesh(geometry, material);
 //adding shadow
 sphere.castShadow = true;
 sphere.receiveShadow = true;
 
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(100, 100),
+    new THREE.PlaneGeometry(200, 200),
     new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
 );
 floor.rotation.x = -Math.PI / 2;
+floor.position.y = -3;
+floor.castShadow = true;
 floor.receiveShadow = true;
 
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(100, 100, 0);
+light.castShadow = true;
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+//blur
+light.shadow.radius = 10;
+
+scene.add(light);
 scene.add(sphere);
-scene.add(hemiLight);
 scene.add(dirLight);
 scene.add(camera);
 scene.add(floor);
 
 render();
 animateSphere();
+
+document.addEventListener('click', fullRotation);
 
 window.addEventListener(
     'resize',
